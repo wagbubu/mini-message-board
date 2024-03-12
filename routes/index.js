a5c758aa20822f852f8e9e78bd906fbe63be4f21
raven <raven@pop-os.localdomain>
2024-03-12 22:47:15 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini Mesageboard', messages: messages });
+});
+router.get('/new', function (req, res, next) {
+  res.render('form');
+});
+router.post('/new', function (req, res) {
+  const newMessage = {
+    text: req.body.messageText,
+    user: req.body.usernameText,
+    added: new Date(),
+  };
+  messages.push(newMessage);
+  res.redirect('/');
+});
+
+module.exports = router;
